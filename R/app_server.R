de4d66bdab02f2299fcbb75eb46054c8399fcb9a
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2025-04-24 20:58:52 -0500

    Initial commit

---
+++ R/app_server.R
@@ -0,0 +1,46 @@
+#' The application server-side
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @import ggplot2
+#' @noRd
+app_server <- function(input, output, session) {
+  # Your application server logic
+  output$data_table <- DT::renderDT({
+    shinipsum::random_DT(5, 5)
+  })
+  output$plot <- shiny::renderPlot({
+
+    bio1_ts <- terra::unwrap(xsdm::cmcc_cm_bio1)
+    bio12_ts <- terra::unwrap(xsdm::cmcc_cm_bio1)
+    envData <- list(bio1 = bio1_ts, bio12 = bio12_ts)
+
+
+    vs_plot <- xsdm::vsp(env_data = envData,
+                   param.list = list(mu =   c(input$mu_1, input$mu_2),
+                                     sigl = c(input$sigl_1, input$sigl_2),
+                                     sigr = c(input$sigr_1, input$sigr_2),
+                                     c =  input$c,
+                                     pd = input$pd,
+                                     L = diag(1, nrow = 2) ))
+
+    ggplot2::ggplot() +
+      stars::geom_stars(data = stars::st_as_stars(vs_plot)) +
+      #ggplot2::scale_fill_continuous(limits = c(0, 1)) +
+      ggplot2::coord_equal() +
+      ggplot2::theme_void() +
+      ggplot2::scale_fill_viridis_c(limits = c(0, 1)) +
+      ggplot2::theme_void() +
+      ggplot2::theme(
+        legend.position = "bottom"
+      )
+
+
+
+  })
+  output$text <- shiny::renderText({
+    shinipsum::random_text(nwords = 50)
+  })
+}
+
---
+++ R/app_ui.R
@@ -0,0 +1,101 @@
+#' The application User-Interface
+#'
+#' @param request Internal parameter for `{shiny}`.
+#'     DO NOT REMOVE.
+#' @import shiny
+#' @noRd
+app_ui <- function(request) {
+  tagList(
+    # Leave this function for adding external resources
+    golem_add_external_resources(),
+    # Your application UI logic
+    fluidPage(
+      titlePanel("VirtualSpecies"),
+      verticalLayout(
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput("mu_1",
+                        "Env 1 mu:",
+                        min = -1,
+                        max = 12,
+                        value = 6),
+            sliderInput("mu_2",
+                        "Env 2 mu:",
+                        min = 0,
+                        max = 592,
+                        value = 300),
+            sliderInput("sigl_1",
+                        "Env 1 sigl:",
+                        min = 0,
+                        max = 1,
+                        value = 0.5),
+            sliderInput("sigl_2",
+                        "Env 2 sigl:",
+                        min = 0,
+                        max = 200,
+                        value = 80),
+            sliderInput("sigr_1",
+                        "Env 1 sigr:",
+                        min = 0,
+                        max = 1,
+                        value = 0.5),
+            sliderInput("sigr_2",
+                        "Env 2 sigr:",
+                        min = 0,
+                        max = 200,
+                        value = 80),
+            sliderInput("c",
+                        "C:",
+                        min = -50,
+                        max = 50,
+                        value =-20),
+            sliderInput("pd",
+                        "PD:",
+                        min = 0,
+                        max = 1,
+                        value = 0.9)
+          ),
+          mainPanel(
+            #h2("A Random DT"),
+            #DT::DTOutput("data_table"),
+            htmltools::h2("Virtual Species"),
+            shiny::plotOutput("plot"),
+            htmltools::h2("A Random Text"),
+            shiny::tableOutput("text")
+          )
+
+
+        ),
+
+      )
+
+
+      #golem::golem_welcome_page() # Remove this line to start building your UI
+    )
+  )
+}
+
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @import shiny
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
+golem_add_external_resources <- function() {
+  add_resource_path(
+    "www",
+    app_sys("app/www")
+  )
+
+  tags$head(
+    favicon(),
+    bundle_resources(
+      path = app_sys("app/www"),
+      app_title = "virtualsp"
+    )
+    # Add here other external resources
+    # for example, you can add shinyalert::useShinyalert()
+  )
+}
