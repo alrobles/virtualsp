1b0b7fa1eff9b7215d4f4a808db36b9c7833bde7
Angel Luis Robles Fernández <a.l.robles.fernandez@gmail.com>
2025-04-30 11:29:54 -0500

    updating bio layers. Adding L matrix slide

--- R/app_server.R
+++ R/app_server.R
@@ -7,40 +7,112 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
-  output$data_table <- DT::renderDT({
-    shinipsum::random_DT(5, 5)
-  })
-  output$plot <- shiny::renderPlot({
 
-    bio1_ts <- terra::unwrap(xsdm::cmcc_cm_bio1)
-    bio12_ts <- terra::unwrap(xsdm::cmcc_cm_bio12)
-    envData <- list(bio1 = bio1_ts, bio12 = bio12_ts)
+  bio1_ts <-  terra::rast("inst/extdata/bio1.tif")
+  bio12_ts <- terra::rast("inst/extdata/bio12.tif")
 
+  envData <- list(bio1 = bio1_ts, bio12 = bio12_ts)
 
-    vs_plot <- xsdm::vsp(env_data = envData,
-                   param.list = list(mu =   c(input$mu_1, input$mu_2),
-                                     sigl = c(input$sigl_1, input$sigl_2),
-                                     sigr = c(input$sigr_1, input$sigr_2),
-                                     c =  input$c,
-                                     pd = input$pd,
-                                     L = diag(1, nrow = 2) ))
+  bio1_mean_ts <-  terra::rast("inst/extdata/bio1_ts_mean.tif")
+  bio12_mean_ts <- terra::rast("inst/extdata/bio12_ts_mean.tif")
 
-    ggplot2::ggplot() +
-      stars::geom_stars(data = stars::st_as_stars(vs_plot)) +
-      #ggplot2::scale_fill_continuous(limits = c(0, 1)) +
-      ggplot2::coord_equal() +
-      ggplot2::theme_void() +
-      ggplot2::scale_fill_viridis_c(limits = c(0, 1)) +
-      ggplot2::theme_void() +
-      ggplot2::theme(
-        legend.position = "bottom"
-      )
+  envData_mean <- list(bio1 = bio1_mean_ts, bio12 = bio12_mean_ts)
 
-
+  vs_plot_data <- reactive({
+    xsdm::vsp(env_data = envData,
+              param.list = list(mu =   c(input$mu_1, input$mu_2),
+                                sigl = c(input$sigl_1, input$sigl_2),
+                                sigr = c(input$sigr_1, input$sigr_2),
+                                c =  input$c,
+                                pd = input$pd,
+                                L = lkj::L_matrix(2, input$L) ))
 
   })
-  output$text <- shiny::renderText({
-    shinipsum::random_text(nwords = 50)
+
+  vs_plot_data_mean <- reactive({
+    xsdm::vsp(env_data = envData_mean,
+              param.list = list(mu =   c(input$mu_1, input$mu_2),
+                                sigl = c(input$sigl_1, input$sigl_2),
+                                sigr = c(input$sigr_1, input$sigr_2),
+                                c =  input$c,
+                                pd = input$pd,
+                                L = lkj::L_matrix(2, input$L) ))
+
+  })
+
+  vs_df_data <- reactive({
+    vs_plot <- vs_plot_data()
+    vs_presence <- terra::app(vs_plot, function(x){
+      ifelse(x > 0.5, x, NA)
+    })
+    df_presence <- terra::spatSample(vs_presence, 200, na.rm = TRUE, xy = TRUE)
+    df_absence <-  terra::spatSample(vs_plot, 200, na.rm = TRUE, xy = TRUE)
+    names(df_presence) <- c("longitude", "latitude", "probs")
+    names(df_absence) <- c("longitude", "latitude", "probs")
+    df_presence$occurrence = rbinom(nrow(df_presence), size = 1,  prob = df_presence$probs)
+    df_absence$occurrence = rbinom(nrow(df_absence), size = 1,  prob = df_absence$probs)
+    df <- rbind(df_presence, df_absence)
+    #shinipsum::random_DT(5, 5)
+
+
+    #df <- df[df$occurrence == 1, ]
+    df
+  })
+
+  output$data_table  <- DT::renderDataTable(
+    DT::datatable(
+      { vs_df_data() },
+
+      extensions = 'Buttons',
+
+      options = list(
+        paging = TRUE,
+        searching = TRUE,
+        fixedColumns = TRUE,
+        autoWidth = TRUE,
+        ordering = TRUE,
+        dom = 'tB',
+        buttons = c('copy', 'csv', 'excel')
+      ),
+
+      class = "display"
+    ))
+
+
+
+
+  output$plot_1 <- shiny::renderPlot({
+    vs_plot <- vs_plot_data()
+    vs_df <- vs_df_data()
+
+    df_presence <- vs_df[vs_df$occurrence == 1, ]
+    df_absence <- vs_df[vs_df$occurrence != 1, ]
+
+    vs_presence_pts <- terra::vect(df_presence, geom = c("longitude", "latitude"))
+    vs_absence_pts <- terra::vect(df_absence, geom = c("longitude", "latitude"))
+
+    terra::plot(vs_plot, range = c(0.1, 1))
+    terra::points(vs_presence_pts, col = "red", cex = 0.2)
+    terra::points(vs_absence_pts, col = "black", cex = 0.2)
+
+
+  })
+
+  output$plot_2 <- shiny::renderPlot({
+    vs_plot_mean <- vs_plot_data_mean()
+    vs_df <- vs_df_data()
+
+    df_presence <- vs_df[vs_df$occurrence == 1, ]
+    df_absence <- vs_df[vs_df$occurrence != 1, ]
+
+    vs_presence_pts <- terra::vect(df_presence, geom = c("longitude", "latitude"))
+    vs_absence_pts <- terra::vect(df_absence, geom = c("longitude", "latitude"))
+
+    terra::plot(vs_plot_mean, range = c(0.1, 1))
+    terra::points(vs_presence_pts, col = "red", cex = 0.2)
+    terra::points(vs_absence_pts, col = "black", cex = 0.2)
+
+
   })
 }
 
